ac726793f236153bee2454bf95ae3c4881061a60
Jonathon Gebhardt <jonathon.gebhardt@gmail.com>
2018-03-07 23:10:43 -0500

    reworked main method and file parsing

--- analyze.py
+++ analyze.py
@@ -3,6 +3,8 @@
 import re
 
 money_pattern = re.compile('\d*[,]*\d+\.\d+')
+deposits = []
+debits = []
 
 def getCollectorString(line):
     result = ""
@@ -11,6 +13,83 @@ def getCollectorString(line):
             result += token + " "
 
     return result.replace("\n", "")
+
+
+def getDeposits(f):
+    money = 0.0
+    date = ""
+    found_money = False
+    
+    line = f.readline()
+    
+    while line:
+        
+        # Break at next section
+        if line == "    OTHER DEBITS\n":
+            break
+        
+        # Deposit/Debit type immediately follows amount
+        # Date is first index
+        # Internal funds transfers have no additional line
+        temp = line.split(" ")
+
+        # if money is found, next line is where money was spent
+        if found_money:
+            collector = getCollectorString(temp)
+            new_line = [getCollectorString(temp), date, money]
+            deposits.append(new_line)
+            found_money = False
+
+        for token in temp:
+            if money_pattern.match(token):
+                # Date is first index
+                date = temp[0]
+                money = float(token.replace(",", "")) # remove commas
+                found_money = True
+        
+
+        line = f.readline()
+    
+    print(len(deposits))
+
+
+def getDebits(f):
+    money = 0.0
+    date = ""
+    found_money = False
+    
+    line = f.readline()
+    
+    while line:
+        
+        # Break at next section
+        if line == " ACCOUNT BALANCE SUMMARY\n":
+            break
+        
+        # Deposit/Debit type immediately follows amount
+        # Date is first index
+        # Internal funds transfers have no additional line
+        temp = line.split(" ")
+
+        # if money is found, next line is where money was spent
+        if found_money:
+            collector = getCollectorString(temp)
+            new_line = [getCollectorString(temp), date, money]
+            debits.append(new_line)
+            found_money = False
+
+        for token in temp:
+            if money_pattern.match(token):
+                # Date is first index
+                date = temp[0]
+                money = float(token.replace(",", "")) # remove commas
+                found_money = True
+        
+
+        line = f.readline()
+    
+    print(len(debits))
+
 
 # TODO: Only call this function if in deposits/debits
 #       Endless loop on prelude
@@ -42,61 +121,19 @@ def contains(arr, collector):
 
     return index
 
+def main():
+    #" ACCOUNT BALANCE SUMMARY\n":
+
+    with open("20180216_raw", "r") as f:
+        line = f.readline()
+        
+        while line:
+            if line == "    DEPOSITS AND OTHER CREDITS\n":
+                break
+            line = f.readline()
+                
+        getDeposits(f)
+        getDebits(f)
 
 
-f = open("20180216_raw", "r")
-
-#money_pattern = re.compile('\d*[,]*\d+\.\d+')
-money = 0.0
-date = ""
-found_money = False
-ledger = []
-
-# Deposit/Debit type immediately follows amount
-# Date is first index
-# Internal funds transfers have no additional line
-
-for line in f:
-    # Marks end of file, no more debits after this
-    if line == " ACCOUNT BALANCE SUMMARY\n":
-        print("--- Account balance summary --- ")
-    elif line == "    DEPOSITS AND OTHER CREDITS\n":
-        print("--- Deposits and other credits ---")
-    elif line == "    OTHER DEBITS\n":
-        print("--- Other Debits --- ")
-    temp = line.split(" ")
-
-    # if money is found, next line is where money was spent
-    if found_money:
-        collector = getCollectorString(temp)
-        new_line = [getCollectorString(temp), date, money]
-        index = contains(ledger, collector)
-
-        # TODO
-        # Check if ledger contains collector
-        # If ledger already has collector, increment money and add date
-        # Otherwise, add new collector
-        if index == -1:
-            ledger.append([collector, date, money])
-            print(str(new_line))
-        else:
-            ledger[index][1] += ", " + date
-            ledger[index][2] += money
-            print(str(ledger[index]))
-            
-#        ledger.append(new_line)
-        found_money = False
-        k = input()
-
-    for token in temp:
-        if money_pattern.match(token):
-            # Date is first index
-            date = temp[0]
-            
-            if "\n" in token: # remove new line escapes
-                token = token[:-1]
-            money = float(token.replace(",", "")) # remove commas
-            found_money = True
-
-print(monies)
-
+main()
